757b420493f3294f296170c0330472ab39e7148b
Antonny41810 <aclinares78@misena.edu.co>
2022-08-27 07:52:11 -0500

    crud eventos

--- views/admin/crearEvento.php
+++ views/admin/crearEvento.php
@@ -11,121 +11,143 @@ if (!isset($_SESSION['rol'])) {
 }
 
 ?>
+<?php
+
+include_once '../../queries/conexion.php';
+
+if (isset($_POST['crear'])) {
+    $responsable = $_POST['responsable'];
+    $objetivo_act = $_POST['objetivo_act'];
+    $area = $_POST['area'];
+    $rama = $_POST['rama'];
+    $fechaInicio = $_POST['fechaInicio'];
+    $fechaFin = $_POST['fechaFin'];
+    $lugar = $_POST['lugar'];
+    $nombre_act = $_POST['nombre_act'];
+    $descri_act = $_POST['descri_act'];
+    $materiales = $_POST['materiales'];
+    $fact_riesgo = $_POST['fact_riesgo'];
+    $evaluacion_act = $_POST['evaluacion_act'];
+    $f_elab_por = $_POST['f_elab_por'];
+    $costo = $_POST['costo'];
+
+    $sql = "INSERT INTO f_actividades (responsable, objetivo_act, area, rama, fechaInicio, fechaFin, lugar, nombre_act, descri_act, materiales, fact_riesgo, evaluacion_act, f_elab_por, costo)  values ('$responsable','$objetivo_act','$area','$rama','$fechaInicio','$fechaFin','$lugar','$nombre_act','$descri_act','$materiales','$fact_riesgo','$evaluacion_act','$f_elab_por','$costo')";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("Location: /proyectoGrupoScout/views/admin/listEventos.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+?>
+
 <title>Crear Evento</title>
 <?php
 
 require '../templates/header.php';
 
 ?>
-<a href="/proyectoGrupoScout/views/admin/menuAdmin.php" class="btn links_nav m-2">Volver</a>
+<a href="/proyectoGrupoScout/views/admin/listEventos.php" class="btn links_nav m-2">Volver</a>
 <div class="container w-75 mt-5 mb-5 container_general">
-        <div class="row align-items-stretch">
-            <div class="col m-auto d-none d-lg-block col-md-5 col-lg-5 col-xl-6">
-                <img src="/proyectoGrupoScout/assets/img/LOGO_GS.png" alt="" width="350" class="d-flex m-auto">
+    <div class="row align-items-stretch">
+        <div class="col m-auto d-none d-lg-block col-md-5 col-lg-5 col-xl-6">
+            <img src="/proyectoGrupoScout/assets/img/LOGO_GS.png" alt="" width="350" class="d-flex m-auto">
+        </div>
+        <div class="col p-3">
+            <div class="row text-center d-block d-sm-block d-md-block d-lg-none">
+                <div class="">
+                    <img src="/proyectoGrupoScout/assets/img/LOGO_GS.png" alt="" width="180" class="img-fluid">
+                </div>
             </div>
-            <div class="col p-3">
-                <div class="row text-center d-block d-sm-block d-md-block d-lg-none">
+            <h2 class="titulo fw-bold text-center py-3">Crear Evento</h2>
+            <!-- formlario registro -->
+            <form action="./crearEvento.php" method="POST" class="p-3 form_registro justify-content-center align-items-center">
+                <div class="row row-cols-md-2 row-cols-sm-1">
                     <div class="">
-                        <img src="/proyectoGrupoScout/assets/img/LOGO_GS.png" alt="" width="180" class="img-fluid">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="responsable" autofocus placeholder="Responsable de la actividad" title="Responsable de la actividad" required>
+                    </div>
+                    <div class="">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="objetivo_act" placeholder="Objetivo" title="Objetivo" required>
+                    </div>
+
+                </div>
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="area" placeholder="Área" title="Área" required>
+                    </div>
+                    <div class="">
+                        <select class="form-select mb-3 fw-bold input_login" name="rama" required title="Seleccione la rama">
+                            <option disabled selected value>Seleccione la rama</option>
+                            <option value="Lobatos">Lobatos</option>
+                            <option value="Scuts">Scouts</option>
+                            <option value="Caminantes">Caminantes</option>
+                            <option value="Rovers">Rovers</option>
+                            <option value="Dirigentes">Dirigentes</option>
+                        </select>
+                    </div>
+
+                </div>
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="">
+                        <input type="datetime-local" class="form-control mb-3 fw-bold input_login" name="fechaInicio" placeholder="Fecha Inicio" data-bs-toggle="tooltip" title="Fecha y hora de inicio" required>
+                    </div>
+                    <div class="">
+                        <input type="datetime-local" class="form-control mb-3 fw-bold input_login" name="fechaFin" placeholder="Fecha Final" data-bs-toggle="tooltip" title="Fecha y hora final" required>
                     </div>
                 </div>
-                <h2 class="titulo fw-bold text-center py-3">Crear actividad</h2>
-                <!-- formlario registro -->
-                <form action="./validaciones/vCrearEvento.php" method="POST" class="p-3 form_registro justify-content-center align-items-center">
-                    <div class="row row-cols-md-2 row-cols-sm-1">
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="responsable" autofocus
-                                placeholder="Responsable de la actividad" title="Responsable de la actividad" required>
-                        </div>
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="objetivo_act"
-                                placeholder="Objetivo" title="Objetivo" required>
-                        </div>
 
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="lugar" placeholder="Lugar" title="Lugar" required>
                     </div>
-                    <div class="row row-cols-md-2 row-cols-sm-1">
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="area"
-                                placeholder="Área" title="Área" required>
-                        </div>
-                        <div class="">
-                            <select class="form-select mb-3 fw-bold input_login" name="rama" required title="Seleccione la rama">
-                                <option disabled selected value>Seleccione la rama</option>
-                                <option value="Lobatos">Lobatos</option>
-                                <option value="Scuts">Scouts</option>
-                                <option value="Caminantes">Caminantes</option>
-                                <option value="Rovers">Rovers</option>
-                                <option value="Dirigentes">Dirigentes</option>
-                            </select>
-                        </div>
+                    <div class="">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="nombre_act" placeholder="Nombre de la actividad" title="Nombrea actividad" required>
+                    </div>
+                </div>
 
+                <div class="row">
+                    <div class="">
+                        <textarea class="form-control mb-3 fw-bold input_login" name="descri_act" placeholder="Descripción actividad..." title="Descripción actividad..." required></textarea>
                     </div>
-                    <div class="row row-cols-md-2 row-cols-sm-1">
-                        <div class="">
-                            <input type="datetime-local" class="form-control mb-3 fw-bold input_login" name="fechaInicio"
-                                placeholder="Fecha Inicio" data-bs-toggle="tooltip" title="Fecha y hora de inicio" required>
-                        </div>
-                        <div class="">
-                            <input type="datetime-local" class="form-control mb-3 fw-bold input_login" name="fechaFin"
-                                placeholder="Fecha Final" data-bs-toggle="tooltip" title="Fecha y hora final" required>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <textarea class="form-control mb-3 fw-bold input_login" name="materiales" placeholder="Materiales..." title="Materiales..." required></textarea>
                     </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <textarea class="form-control mb-3 fw-bold input_login" name="fact_riesgo" placeholder="Factores de riesgo" title="Factores de riesgo" required></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <textarea class="form-control mb-3 fw-bold input_login" name="evaluacion_act" placeholder="Evaluación actividad" title="Evaluación actividad" required></textarea>
+                    </div>
+                </div>
 
-                    <div class="row row-cols-md-2 row-cols-sm-1">
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="lugar"
-                                placeholder="Lugar" title="Lugar" required>
-                        </div>
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="nombre_act"
-                                placeholder="Nombre de la actividad" title="Nombrea actividad" required>
-                        </div>
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="">
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="f_elab_por" placeholder="Actividad elaborada por" title="Actividad elaborada por" required>
                     </div>
+                    <div class="">
+                        <input type="number" class="form-control mb-3 fw-bold input_login" name="costo" placeholder="Costo actividad" title="Costo actividad" required>
+                    </div>
+                </div>
 
-                    <div class="row">
-                        <div class="">
-                            <textarea class="form-control mb-3 fw-bold input_login" name="descri_act"
-                                placeholder="Descripción actividad..." title="Descripción actividad..." required></textarea>
-                        </div>
+                <div class="row">
+                    <div class="col d-flex justify-content-center align-items-center p-2">
+                        <button type="submit" class="btn btn_general" name="crear">Crear Evento</button>
                     </div>
-                    <div class="row">
-                        <div class="">
-                            <textarea class="form-control mb-3 fw-bold input_login" name="materiales"
-                                placeholder="Materiales..." title="Materiales..." required></textarea>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="">
-                            <textarea class="form-control mb-3 fw-bold input_login" name="fact_riesgo"
-                                placeholder="Factores de riesgo" title="Factores de riesgo" required></textarea>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="">
-                            <textarea class="form-control mb-3 fw-bold input_login" name="evaluacion_act"
-                                placeholder="Evaluación actividad" title="Evaluación actividad" required></textarea>
-                        </div>
-                    </div>
-
-                    <div class="row row-cols-md-2 row-cols-sm-1">
-                        <div class="">
-                            <input type="text" class="form-control mb-3 fw-bold input_login" name="f_elab_por"
-                                placeholder="Actividad elaborada por" title="Actividad elaborada por" required>
-                        </div>
-                        <div class="">
-                            <input type="number" class="form-control mb-3 fw-bold input_login" name="costo"
-                                placeholder="Costo actividad" title="Costo actividad" required>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col d-flex justify-content-center align-items-center p-2">
-                            <button type="submit" class="btn btn_general">Crear actividad</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-<a href="/proyectoGrupoScout/views/admin/menuAdmin.php">Volver</a>
+<?php
+
+require '../templates/footer.php';
+
+?>
---
+++ views/admin/listEventos.php
@@ -0,0 +1,95 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['rol'])) {
+  header("Location: ../login.php");
+} else {
+  if ($_SESSION['rol'] != 1) {
+    header("Location: ../login.php");
+  }
+}
+
+?>
+
+<?php
+include_once '../../queries/conexion.php';
+
+$evento = "SELECT * FROM f_actividades";
+$result = mysqli_query($conn, $evento);
+?>
+
+<title>Eventos</title>
+
+<?php
+
+require '../templates/header.php';
+
+?>
+
+<a href="/proyectoGrupoScout/views/admin/menuAdmin.php" class="btn links_nav m-2">Volver</a>
+
+<div class="container bg-light p-3 containerCrud mb-3">
+  <h1 class="titulo fw-bold text-center m-3">Lista de eventos</h1>
+  <a class="mb-3 btn crearNuevo" href="/proyectoGrupoScout/views/admin/crearEvento.php">Crear nuevo</a>
+  <table class="table table-borderless table-bordered" style="border-radius: 5px;">
+    <thead class="cabeceraTablas text-center">
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Lugar</th>
+        <th scope="col">Costo</th>
+        <th scope="col">Fecha-Inicio</th>
+        <th scope="col">Fecha-Fin</th>
+        <th scope="col">Acciones</th>
+      </tr>
+    </thead>
+    <tbody class="text-center">
+      <?php
+
+      while ($mostrar = mysqli_fetch_array($result)) {
+      ?>
+        <tr>
+          <td><?php echo $mostrar['id_act'] ?>
+          <td><?php echo $mostrar['nombre_act'] ?>
+          <td><?php echo $mostrar['lugar'] ?>
+          <td>$<?php echo $mostrar['costo'] ?>
+          <td><?php echo $mostrar['fechaInicio'] ?>
+          <td><?php echo $mostrar['fechaFin'] ?>
+          <td class="text-center">
+            <a class="m-1 btn btnDetalles" href="./detalleEvento.php?id=<?php echo $mostrar['id_act'] ?>"> Detalles</a>
+            <a class="m-1 btn btnEditar" href="./editarEvento.php?id=<?php echo $mostrar['id_act'] ?>"> Editar</a>
+            <button type="button" class="m-1 btn btnEliminar" data-bs-toggle="modal" data-bs-target="#mEliminar<?php echo $mostrar['id_act'] ?>">Eliminar</button>
+          </td>
+        </tr>
+        <!-- Modal -->º
+        <div class="modal fade" id="mEliminar<?php echo $mostrar['id_act'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Notificación</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ¿Desea eliminar este evento?
+                <p><?php echo $mostrar['nombre_act'] . ', ' . $mostrar['f_elab_por'] ?></p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btnCerrar" data-bs-dismiss="modal">Cerrar</button>
+                <a href="" class="btn links_nav">Eliminar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
+
+<?php
+
+require '../templates/footer.php';
+
+?>
