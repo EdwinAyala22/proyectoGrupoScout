7f9075ae464832636733a5ec5fe78d785e7274eb
sGuelpaz <jhon.guelpaz@misena.edu.co>
2022-10-27 21:45:14 -0500

    Editar progresiones sin terminar

--- views/admin/editarProgresion.php
+++ views/admin/editarProgresion.php
@@ -10,11 +10,40 @@ if (!isset($_SESSION['rol'])) {
     }
 }
 
+include_once '../../queries/conexion.php';
+$documento = $_POST['documento'];
+$id_t_adelanto = $_POST['id_t_adelanto'];
+
+if (isset($_GET['idAct'])) {
+    $
+    $queryU = "UPDATE FROM  segui_plan_adelanto set  WHERE $documento = '$id_act'";
+    /*UPDATE f_actividades set responsable = '$responsable', objetivo_act = '$objetivo_act', area = '$area', fechaInicio = 
+    '$fechaIniciio', fechaFin = '$fechaFiin', lugar = '$lugar', nombre_act = '$nombre_act', descri_act = '$descri_act', materiales = '$materiales', fact_riesgo = '$fact_riesgo',
+     evaluacion_act = '$evaluacion_act',
+     f_elab_por = '$f_elab_por', costo = '$costo' WHERE id_act = $id"*/
+    $result = mysqli_query($conn, $query);
+ 
+    } else {
+
+    }
+    
+    
+
+$query1 = "SELECT * FROM usuarios U, segui_plan_adelanto S, tipodeadelanto T WHERE U.documento = S.documento AND S.id_t_adelanto = T.id_t_adelanto AND S.documento = $documento AND S.id_t_adelanto = $id_t_adelanto";
+$result1 = mysqli_query($conn, $query1);
+$nr = mysqli_num_rows($result1);
+$mostrar = mysqli_fetch_array($result1);
+
+$rama = $mostrar['id_rama'];
+$query2 = "SELECT * FROM ramas WHERE id_rama = $rama";
+$result2 = mysqli_query($conn, $query2);
+$mostrarR = mysqli_fetch_array($result2);
+
+
 ?>
 <title>Editar progresión</title>
 <?php
 
-include_once '../../queries/conexion.php';
 
 
 require '../templates/header.php';
@@ -54,33 +83,51 @@ $result = mysqli_query($conn, $query);
             <form action="/proyectoGrupoScout/views/admin/crearPlandeProgresion.php" method="POST" class="p-3 form_registro justify-content-center align-items-center">
                 <div class="row row-cols-md-2 row-cols-sm-1">
                     <div class="">
-                        <input type="text" class="form-control mb-3 fw-bold input_login" name="nombres" autofocus placeholder="Nombres" data-bs-toggle="tooltip" data-bs-placement="top" title="Nombre" required>
+                    <label for="costo" class="form-label fw-bold titulo">Nombres: </label>
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="nombres" autofocus placeholder="Nombres" data-bs-toggle="tooltip" data-bs-placement="top" title="Nombre" value="<?php echo $mostrar['nombres'] ?>" required>
                     </div>
                     <div class="">
-                        <input type="text" class="form-control mb-3 fw-bold input_login" name="apellidos" placeholder="Apellidos" data-bs-toggle="tooltip" data-bs-placement="top" title="Apellidos" required>
+                    <label for="costo" class="form-label fw-bold titulo">Apellido: </label>
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="apellidos" placeholder="Apellidos" data-bs-toggle="tooltip" data-bs-placement="top" title="Apellidos" value="<?php echo $mostrar['apellido1'] ?>" required>
                     </div>
 
                 </div>
 
                 <div class="row row-cols-md-2 row-cols-sm-1">
                     <div class="">
-                        <input type="number" class="form-control mb-3 fw-bold input_login" name="documento" placeholder="No. de documento" data-bs-toggle="tooltip" data-bs-placement="top" title="Número de documento" required>
+                    <label for="costo" class="form-label fw-bold titulo">No. de documento: </label>
+                        <input type="number" class="form-control mb-3 fw-bold input_login" name="documento" placeholder="No. de documento" data-bs-toggle="tooltip" data-bs-placement="top" title="Número de documento" value="<?php echo $mostrar['documento'] ?>" readonly>
                     </div>
-                    <div class="form-floating">
-                        <input type="date" class="form-control mb-3 fw-bold input_login" name="fechaEntrega" placeholder="Fecha de entrega" data-bs-toggle="tooltip" data-bs-placement="top" title="Fecha de entrega" required>
-                        <label class="ms-2 fw-bold titulo" for="floatingInput">Fecha de entrega </label>
+                    <div class="">
+                    <label for="costo" class="form-label fw-bold titulo">Fecha de entrega: </label>
+                        <input type="date" class="form-control mb-3 fw-bold input_login" name="fechaEntrega" placeholder="Fecha de entrega" data-bs-toggle="tooltip" data-bs-placement="top" title="Fecha de entrega" value="<?php echo $mostrar['fechaEntrega'] ?>" required>
+                        
                     </div>
                 </div>
+                
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="">
+                    <label for="costo" class="form-label fw-bold titulo">Rama anterior:</label>
+                    <?php echo $mostrarR['nom_rama'] ?></span>
+                    </div>
 
+                    
+                    <div class="" >
+                    <label for="costo" class="form-label fw-bold titulo">Tipo de progresión anterior:</label>
+                    <?php echo $mostrar['nombreTipoAdelanto']  ?>
+
+                    </div>
+
+                </div>
                 <div class="row row-cols-md-2 row-cols-sm-1">
                     <div class="">
                         <!--COMBO BOX RAMA -->
                         <select id="rama_progresion" class="form-select mb-3 fw-bold input_login" name="rama_progresion" required data-bs-toggle="tooltip" data-bs-placement="top" title="Seleccione la rama">
-                            <option disabled selected value>Seleccionar rama</option>
+                            <option disabled selected value>Seleccionar nueva rama</option>
                             <?php
-                            while ($mostrar = mysqli_fetch_array($result)) { ?>
+                            while ($mostrar2 = mysqli_fetch_array($result)) { ?>
 
-                                <option value="<?php echo $mostrar['id_rama']; ?>"><?php echo $mostrar['nom_rama']; ?></option>
+                                <option value="<?php echo $mostrar2['id_rama']; ?>"><?php echo $mostrar2['nom_rama']; ?></option>
 
 
                             <?php
@@ -90,6 +137,7 @@ $result = mysqli_query($conn, $query);
 
                     </div>
 
+                    
                     <div class="" id="progresion-seleccionada">
                         <!-- COMBOBOX PROGRESIÓN-->
                         <!-- <select id="progresion-seleccionada" class="form-select mb-3 fw-bold input_login" name="progresion-seleccionada" required data-bs-toggle="tooltip" data-bs-placement="top" title="Progresión">
@@ -103,19 +151,21 @@ $result = mysqli_query($conn, $query);
                 </div>
                 <div class="row row-cols-md-2 row-cols-sm-1">
                     <div class="">
-                        <input type="text" class="form-control mb-3 fw-bold input_login" name="lugarEntrega" placeholder="Lugar de entrega" data-bs-toggle="tooltip" data-bs-placement="top" title="Lugar de entrega" required>
+                        <label for="lugarEntrega" class="form-label fw-bold titulo">Lugar de entrega: </label>
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="lugarEntrega" placeholder="Lugar de entrega" data-bs-toggle="tooltip" data-bs-placement="top" title="Lugar de entrega" required value="<?php echo $mostrar['lugarEntrega'] ?>" readonly>
                     </div>
                     <div class="">
-                        <input type="text" class="form-control mb-3 fw-bold input_login" name="dirigente" placeholder="Dirigente a cargo" data-bs-toggle="tooltip" data-bs-placement="top" title="Dirigente a cargo" required>
+                        <label for="dirigente" class="form-label fw-bold titulo">Dirigente a cargo: </label>
+                        <input type="text" class="form-control mb-3 fw-bold input_login" name="dirigente" placeholder="Dirigente a cargo" data-bs-toggle="tooltip" data-bs-placement="top" title="Dirigente a cargo" required value="<?php echo $mostrar['dirigente'] ?>" readonly>
                     </div>
                 </div>
 
                 <div class="row ">
                     <div class="">
-                        <input type="Number" class="form-control mb-3 fw-bold input_login" name="costo" placeholder="Costo" data-bs-toggle="tooltip" data-bs-placement="top" title="Costo" required>
+                    <label for="costo" class="form-label fw-bold titulo">Costo: </label>
+                        <input type="Number" class="form-control mb-3 fw-bold input_login" name="costo" placeholder="Costo" data-bs-toggle="tooltip" data-bs-placement="top" title="Costo" value="<?php echo $mostrar['costo'] ?>" required>
                     </div>
                 </div>
-
                 <div class="row">
                     <div class="col d-flex justify-content-center align-items-center p-2">
                         <button type="submit" name="crearP" class="btn btn_general">Editar seguimiento</button>
